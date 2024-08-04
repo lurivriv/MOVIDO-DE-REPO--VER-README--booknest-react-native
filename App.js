ed5309d09e4171e72a6cf805245c8156a3a4a100
Lucila Riviezzi <120687515+lurivriv@users.noreply.github.com>
2024-08-04 19:51:23 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,17 @@
+import { Navigator } from "./src/navigation/Navigator.jsx"
 
+export default function App() {
+  StatusBar.setBackgroundColor(colors.black)
+  StatusBar.setBarStyle("light-content")
+
+  const [fontsLoaded, fontError] = useFonts({
+    "Roboto-regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-regular-italic": require("./assets/fonts/Roboto-Italic.ttf"),
+	@@ -27,7 +25,7 @@ export default function App() {
+      <Provider store={store}>
+        <Navigator />
+      </Provider>
+      <Toast />
+    </SafeAreaView>
+  )
+}
